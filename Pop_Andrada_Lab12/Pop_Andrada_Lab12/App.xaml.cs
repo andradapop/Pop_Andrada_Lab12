a5fdf36f359d8e7b8d364514c192a99933439214
ANDRADA-ROBERTA POP <andrada.roberta.pop@stud.ubbcluj.ro>
2021-01-06 17:27:15 +0200

    Add project files.

---
+++ Pop_Andrada_Lab12/Pop_Andrada_Lab12/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Pop_Andrada_Lab12.Data;
+
+namespace Pop_Andrada_Lab12
+{
+    public partial class App : Application
+    {
+        
+         public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            //InitializeComponent();
+
+            //  MainPage = new MainPage();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
